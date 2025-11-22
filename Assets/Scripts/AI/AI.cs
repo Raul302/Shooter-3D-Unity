baf4686a57c7f7eda3ec41ec7b2a3b6922b83db3
Raul <luis_1547@hotmail.com>
2025-11-22 13:20:34 -0600

    Ammo Box Object
    Adding Ammo Box

--- Assets/Scripts/AI/AI.cs
+++ Assets/Scripts/AI/AI.cs
@@ -1,28 +1,73 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.Controls;
 public class AI : MonoBehaviour
 {
 
     public NavMeshAgent navMeshAgent;
 
-    public GameObject destiny_point;
-    public GameObject second_destiny_point;
+
+    public Transform[] destinations;
+
+    private int i = 0;
+
+    [Header ( " --- FOLLOW PLAYER ----")]
+    public  bool follow_player;
+
+    private GameObject player;
+
+    private float distance_to_player;
+
+    public float distance_to_follow = 10;
+
+  
 
     private void Start()
     {
-        navMeshAgent.destination = destiny_point.transform.position;
+        navMeshAgent.destination = destinations[0].transform.position;
+        player = FindAnyObjectByType<PlayerMovement>().gameObject;
+
 
     }
 
     private void Update()
     {
-        float distance = Vector3.Distance(transform.position,destiny_point.transform.position);
+        distance_to_player =
+            Vector3.Distance(transform.position, player.transform.position);
 
-        if (distance < 2)
+        if (distance_to_player <= distance_to_follow && follow_player )
         {
-            navMeshAgent.destination = second_destiny_point.transform.position;
-
+            FollowPlayer();
+        } else
+        {
+            EnemyPath();
         }
+       
+    }
+
+
+    public void EnemyPath()
+    {
+        navMeshAgent.destination = destinations[i].transform.position;
+
+        if (Vector3.Distance(transform.position, destinations[i].position) <= 2)
+        {
+            if (destinations[i] != destinations[destinations.Length - 1])
+            {
+                i = i + 1;
+            } else
+            {
+                i = 0;
+
+            }
+        }
+
+    }
+
+    public void FollowPlayer()
+    {
+        navMeshAgent.destination = player.transform.position;
     }
 
 }
+ 
---
+++ Assets/Scripts/AmmoBox/AmmoBox.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+
+    public int ammo = 10;
+}
